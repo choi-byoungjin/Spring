b92c457c85370c40d40583e887df15c140e7435d
choi-byoungjin <choibyoungjin85@gmail.com>
2022-04-19 12:03:32 +0900

    스프링 프로젝트 재생성

---
+++ Board/src/main/java/com/spring/employees/controller/EmpController.java
@@ -0,0 +1,10 @@
+package com.spring.employees.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmpController {
+
+//	EmpController empController = new EmpController();
+	
+}
---
+++ Board/src/main/webapp/resources/SQL/spring_board.sql
@@ -0,0 +1,13 @@
+------ ***** spring 기초 ***** ------
+
+show user;
+-- USER이(가) "MYMVC_USER"입니다.
+
+create table spring_test
+(no         number
+,name       varchar2(100)
+,writeday   date default sysdate
+);
+
+select *
+from spring_test;
